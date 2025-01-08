ed28e1d44c7c4404c9a9e3f5ce0c1f867694f083
Anyue <moonwong.way@gmail.com>
2025-01-08 22:56:18 +0100

    add: graphql schema for task

--- src/graphql/typeDefs/taskTypeDefs.ts
+++ src/graphql/typeDefs/taskTypeDefs.ts
@@ -1,1 +1,16 @@
+import { gql } from "apollo-server-express";
 
+export const taskTypeDefs = gql`
+  type Task {
+    id: ID!
+    title: String!
+    description: String
+    status: String
+    created_at: String
+    updated_at: String
+  }
+
+  type Query {
+    getTasks: [Tasks!]!
+  }
+`;
